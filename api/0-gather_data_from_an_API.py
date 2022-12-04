6d3066a5c68ed9251c7543295cbf55fcc32c7383
TotiFlame <santifer4321@gmail.com>
2022-12-04 19:28:40 +0000

    Task 0

---
+++ api/0-gather_data_from_an_API.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Write a Python script that, for a given employee ID,
+returns information about his/her TODO list progress.
+"""
+
+import urllib.request
+import sys
+import json
+
+request_url = urllib.request.urlopen('https://jsonplaceholder.typicode.com/users/{}'.format(sys.argv[1]))
+response = request_url.read()
+user = json.loads(response.decode('utf-8'))
+
+request_url = urllib.request.urlopen('https://jsonplaceholder.typicode.com/todos?userId={}'.format(sys.argv[1]))
+response = request_url.read()
+todos = json.loads(response.decode('utf-8'))
+
+done_tasks = list(filter(lambda todo: todo['completed'], todos))
+
+print("Employee {} is done with tasks({}/{}):".format(user['name'], len(done_tasks), len(todos)))
+for todo in done_tasks:
+    print("\t{}".format(todo['title']))
