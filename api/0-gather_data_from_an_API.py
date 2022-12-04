144532bbf63ed0466a69cc7284a8fa7de4408631
TotiFlame <santifer4321@gmail.com>
2022-12-04 19:40:07 +0000

    Task 0 PEP8

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -6,20 +6,23 @@ returns information about his/her TODO list progress.
 
 
 if __name__ == "__main__":
+    import json
     import urllib.request
     import sys
-    import json
 
-    request_url = urllib.request.urlopen("https://jsonplaceholder.typicode.com/users/{}".format(sys.argv[1]))
+    request_url = urllib.request.urlopen("https://jsonplaceholder.typicode."
+                                         "com/users/{}".format(sys.argv[1]))
     response = request_url.read()
     user = json.loads(response.decode("utf-8"))
 
-    request_url = urllib.request.urlopen("https://jsonplaceholder.typicode.com/todos?userId={}".format(sys.argv[1]))
+    request_url = urllib.request.urlopen("https://jsonplaceholder.typicode."
+                                         "com/todos?userId={}".format(sys.argv[1]))
     response = request_url.read()
     todos = json.loads(response.decode("utf-8"))
 
     done_tasks = list(filter(lambda todo: todo["completed"], todos))
 
-    print("Employee {} is done with tasks({}/{}):".format(user["name"], len(done_tasks), len(todos)))
+    print("Employee {} is done with tasks({}/{}):".format(
+        user["name"], len(done_tasks), len(todos)))
     for todo in done_tasks:
         print("\t {}".format(todo["title"]))
