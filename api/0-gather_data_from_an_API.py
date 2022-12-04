6e08396403f9f07b1bf35180a34ca90fdd1916aa
TotiFlame <santifer4321@gmail.com>
2022-12-04 19:30:39 +0000

    Task 0 documentation

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -8,16 +8,16 @@ import urllib.request
 import sys
 import json
 
-request_url = urllib.request.urlopen('https://jsonplaceholder.typicode.com/users/{}'.format(sys.argv[1]))
+request_url = urllib.request.urlopen("https://jsonplaceholder.typicode.com/users/{}".format(sys.argv[1]))
 response = request_url.read()
-user = json.loads(response.decode('utf-8'))
+user = json.loads(response.decode("utf-8"))
 
-request_url = urllib.request.urlopen('https://jsonplaceholder.typicode.com/todos?userId={}'.format(sys.argv[1]))
+request_url = urllib.request.urlopen("https://jsonplaceholder.typicode.com/todos?userId={}".format(sys.argv[1]))
 response = request_url.read()
-todos = json.loads(response.decode('utf-8'))
+todos = json.loads(response.decode("utf-8"))
 
-done_tasks = list(filter(lambda todo: todo['completed'], todos))
+done_tasks = list(filter(lambda todo: todo["completed"], todos))
 
-print("Employee {} is done with tasks({}/{}):".format(user['name'], len(done_tasks), len(todos)))
+print("Employee {} is done with tasks({}/{}):".format(user["name"], len(done_tasks), len(todos)))
 for todo in done_tasks:
-    print("\t{}".format(todo['title']))
+    print("\t{}".format(todo["title"]))
