5264fb983a402a6c1dfac689a0a00c7d7638d399
TotiFlame <santifer4321@gmail.com>
2022-12-05 19:02:58 +0000

    Task 2

---
+++ api/2-export_to_JSON.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+Write a Python script that, for a given employee ID,
+returns information about his/her TODO list progress.
+"""
+
+
+if __name__ == "__main__":
+    import json
+    import sys
+    import urllib.request
+
+    request_url = urllib.request.urlopen("https://jsonplaceholder.typicode."
+                                         "com/users/{}".format(sys.argv[1]))
+    response = request_url.read()
+    user = json.loads(response.decode("utf-8"))
+
+    request_url = urllib.request.urlopen("https://jsonplaceholder.typicode.com"
+                                         "/todos?userId={}"
+                                         .format(sys.argv[1]))
+    response = request_url.read()
+    tasks = json.loads(response.decode("utf-8"))
+
+    file = open("{}.JSON".format(user["id"]), "w")
+    
+    id = "{}".format(user["id"])
+    
+    userj = { id: [] }
+    
+    for task in tasks:
+        userj[id].append({"task": task["title"],
+                          "completed": task["completed"],
+                          "username": user["username"]}) 
+
+    file.write(json.dumps(userj))
