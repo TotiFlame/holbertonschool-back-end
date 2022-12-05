8fd34b961d0f71fb3c4eefe5114b4a39e974bd4c
Santiago Fernández <105667854+TotiFlame@users.noreply.github.com>
2022-12-04 22:18:50 -0300

    Create 1-export_to_CSV.py

---
+++ api/1-export_to_CSV.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+Write a Python script that, for a given employee ID,
+returns information about his/her TODO list progress.
+"""
+
+
+if __name__ == "__main__":
+    import json
+    import sys
+    import urllib.request
+
+    request_url = urllib.request.urlopen("https://jsonplaceholder.typicode."
+                                         "com/users/{}".format(sys.argv[1]))
+    response = request_url.read()
+    user = json.loads(response.decode("utf-8"))
+
+    request_url = urllib.request.urlopen("https://jsonplaceholder.typicode.com"
+                                         "/todos?userId={}"
+                                         .format(sys.argv[1]))
+    response = request_url.read()
+    tasks = json.loads(response.decode("utf-8"))
+
+    file = open("{}.csv".format(user["id"]), "w")
+
+    for i, j in enumerate(tasks):
+        print("\"{}\",\"{}\",\"{}\",\"{}\"\n".format(user["id"],
+                                                     user["username"],
+                                                     j["completed"],
+                                                     j["title"]))
